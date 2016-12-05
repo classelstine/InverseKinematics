a0560b4e1874ae0b56c4b95901eb78fd7d6aff76
James Musk <muskjames@gmail.com>
2016-12-05 15:08:46 -0800

    set up .h file

--- src/iksolver.h
+++ src/iksolver.h
@@ -1,26 +1,45 @@
 using namespace std;
+#include <Eigen/Dense>
+glm::vec3 goal_pos;
 
-struct shape {
-    vector<glm::vec3> vertices;
-    vector<glm::vec3> normals;
-    bool has_normals;
-};
 
-struct Segment {
-    glm::vec3 in_point;
-    glm::vec3 out_point;
+class Segment {
+    Segment parent;
+    Segment child;
+    glm::vec3 in_joint;
+    glm::vec3 out_joint;
     glm::vec3 in_theta;
-    glm::vec3 out
+    float length;
+
+    Segment () {
+        in_joint = glm::vec3(0,0,0): 
+        out_joint = glm::vec3(0,0,0): 
+        length = 1; 
+    } 
     
 }
 
 class Arm {
+    public:
+        Segment root;
+    
+    //Default 4 segments
+    Arm() { 
+        root = Segment 
+    }
+        
 }
 
 
 
 
-bool close_enough(glm::vec3 p1, glm::vec3 p2, float epsilon);
+bool close_enough(glm::vec3 p1, glm::vec3] p2, float epsilon);
+void initialize_goal(void);
+void update_goal(float path_mode);
+void initialize_arm(void);
+void render(void);
+float update_position(float epsilon, float step_size);
+glm::mat4 getJacobian(
 
 
 
